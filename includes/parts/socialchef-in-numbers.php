20f6c8017e668bc7e731bc26687728e3be893642
boutitinizar <boutitinizar@gmail.com>
2016-03-31 10:38:03 +0100

    deactivation de batimen en chifre

--- includes/parts/socialchef-in-numbers.php
+++ includes/parts/socialchef-in-numbers.php
@@ -25,83 +25,85 @@
 		$forum_stats = bbp_get_statistics();
 	}
 ?>
-				<!--content-->
-				<section class="content full-width">
-					<div class="icons dynamic-numbers">
-						<header class="s-title">
-							<h2 class="ribbon large"><?php echo sprintf(__('%s en chiffres', 'socialchef'), get_bloginfo('name')); ?></h2>
-						</header>
-						
-						<!--row-->
-						<div class="row">
-							<!--item-->
-							<div class="one-fifth">
-								<div class="container">
-									<i class="ico i-members"></i>
-									<span class="title dynamic-number" data-dnumber="<?php echo esc_attr( $total_members ); ?>">0</span>
-									<span class="subtitle"><?php _e('membres', 'socialchef'); ?></span>
-								</div>
-							</div>
-							<!--//item-->
-							
-							<!--item-->
-							<div class="one-fifth">
-								<div class="container">
-									<i class="ico i-recipes"></i>
-									<span class="title dynamic-number" data-dnumber="<?php echo esc_attr( $published_recipes); ?>">0</span>
-									<span class="subtitle"><?php _e('offres', 'socialchef'); ?></span>
-								</div>
-							</div>
-							<!--//item-->
-							
-							<!--item-->
-							<div class="one-fifth">
-								<div class="container">
-									<i class="ico i-photos"></i>
-									<span class="title dynamic-number" data-dnumber="<?php echo esc_attr( $total_attachments ); ?>">0</span>
-									<span class="subtitle"><?php _e('photos', 'socialchef'); ?></span>
-								</div>
-							</div>
-							<!--//item-->
-							
-							<?php if (isset($forum_stats)) { ?>
-							<!--item-->
-							<div class="one-fifth">
-								<div class="container">
-									<i class="ico i-posts"></i>
-									<span class="title dynamic-number" data-dnumber="<?php echo esc_attr( $forum_stats['topic_count'] ); ?>">0</span>
-									<span class="subtitle"><?php _e('forum posts', 'socialchef'); ?></span>
-								</div>
-							</div>
-							<!--//item-->
-							<?php } ?>
-							
-							<!--item-->
-							<div class="one-fifth">
-								<div class="container">
-									<i class="ico i-comment"></i>
-									<span class="title dynamic-number" data-dnumber="<?php echo esc_attr( $approved_comments ); ?>">0</span>
-									<span class="subtitle"><?php _e('commentaires', 'socialchef'); ?></span>
-								</div>
-							</div>
-							<!--//item-->
-							
-							<!--item-->
-							<div class="one-fifth ">
-								<div class="container">
-									<i class="ico i-articles"></i>
-									<span class="title dynamic-number" data-dnumber="<?php echo esc_attr( $published_posts ); ?>">0</span>
-									<span class="subtitle"><?php _e('articles', 'socialchef'); ?></span>
-								</div>
-							</div>
-							<!--//item-->
-							<?php if ( !is_user_logged_in() ) { ?>
-							<div class="cta">
-								<a href="<?php echo esc_url ($sc_theme_globals->get_register_page_url() ); ?>" class="button big"><?php _e('Join us!', 'socialchef'); ?></a>
-							</div>
-							<?php } ?>
-						</div>
-						<!--//row-->
-					</div>
-				</section>
-				<!--//content-->
+<?php /*
+                <!--content-->
+                <section class="content full-width">
+                    <div class="icons dynamic-numbers">
+                        <header class="s-title">
+                            <h2 class="ribbon large"><?php echo sprintf(__('%s en chiffres', 'socialchef'), get_bloginfo('name')); ?></h2>
+                        </header>
+
+                        <!--row-->
+                        <div class="row">
+                            <!--item-->
+                            <div class="one-fifth">
+                                <div class="container">
+                                    <i class="ico i-members"></i>
+                                    <span class="title dynamic-number" data-dnumber="<?php echo esc_attr( $total_members ); ?>">0</span>
+                                    <span class="subtitle"><?php _e('membres', 'socialchef'); ?></span>
+                                </div>
+                            </div>
+                            <!--//item-->
+
+                            <!--item-->
+                            <div class="one-fifth">
+                                <div class="container">
+                                    <i class="ico i-recipes"></i>
+                                    <span class="title dynamic-number" data-dnumber="<?php echo esc_attr( $published_recipes); ?>">0</span>
+                                    <span class="subtitle"><?php _e('offres', 'socialchef'); ?></span>
+                                </div>
+                            </div>
+                            <!--//item-->
+
+                            <!--item-->
+                            <div class="one-fifth">
+                                <div class="container">
+                                    <i class="ico i-photos"></i>
+                                    <span class="title dynamic-number" data-dnumber="<?php echo esc_attr( $total_attachments ); ?>">0</span>
+                                    <span class="subtitle"><?php _e('photos', 'socialchef'); ?></span>
+                                </div>
+                            </div>
+                            <!--//item-->
+
+                            <?php if (isset($forum_stats)) { ?>
+                            <!--item-->
+                            <div class="one-fifth">
+                                <div class="container">
+                                    <i class="ico i-posts"></i>
+                                    <span class="title dynamic-number" data-dnumber="<?php echo esc_attr( $forum_stats['topic_count'] ); ?>">0</span>
+                                    <span class="subtitle"><?php _e('forum posts', 'socialchef'); ?></span>
+                                </div>
+                            </div>
+                            <!--//item-->
+                            <?php } ?>
+
+                            <!--item-->
+                            <div class="one-fifth">
+                                <div class="container">
+                                    <i class="ico i-comment"></i>
+                                    <span class="title dynamic-number" data-dnumber="<?php echo esc_attr( $approved_comments ); ?>">0</span>
+                                    <span class="subtitle"><?php _e('commentaires', 'socialchef'); ?></span>
+                                </div>
+                            </div>
+                            <!--//item-->
+
+                            <!--item-->
+                            <div class="one-fifth ">
+                                <div class="container">
+                                    <i class="ico i-articles"></i>
+                                    <span class="title dynamic-number" data-dnumber="<?php echo esc_attr( $published_posts ); ?>">0</span>
+                                    <span class="subtitle"><?php _e('articles', 'socialchef'); ?></span>
+                                </div>
+                            </div>
+                            <!--//item-->
+                            <?php if ( !is_user_logged_in() ) { ?>
+                            <div class="cta">
+                                <a href="<?php echo esc_url ($sc_theme_globals->get_register_page_url() ); ?>" class="button big"><?php _e('Join us!', 'socialchef'); ?></a>
+                            </div>
+                            <?php } ?>
+                        </div>
+                        <!--//row-->
+                    </div>
+                </section>
+                <!--//content-->
+*/?>
